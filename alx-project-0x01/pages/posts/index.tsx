7249023e204731691d32a90ba54b965d57d5259a
Brianryanphiri <105008060+Brianryanphiri@users.noreply.github.com>
2025-07-18 11:52:26 +0000

    Add navigation header and link all pages

--- alx-project-0x01/pages/posts/index.tsx
+++ alx-project-0x01/pages/posts/index.tsx
@@ -1,7 +1,12 @@
+import Header from "@/components/layout/Header";
+
 const PostsPage: React.FC = () => {
   return (
-    <div className="p-8">
-      <h1 className="text-3xl font-bold">Posts Page</h1>
+    <div>
+      <Header />
+      <div className="p-8">
+        <h1 className="text-3xl font-bold">Posts Page</h1>
+      </div>
     </div>
   );
 };
