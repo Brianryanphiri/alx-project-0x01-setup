3f06c342690ca922b769dcc5c546c57f2fedc2b1
Brianryanphiri <105008060+Brianryanphiri@users.noreply.github.com>
2025-07-19 19:45:42 +0000

    fix(users): rename posts to users and use users.map in Users page for dynamic rendering

--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -1,23 +1,26 @@
+import { useState } from "react";
 import Header from "@/components/layout/Header";
 import UserCard from "@/components/common/UserCard";
 import { UserProps } from "@/interfaces";
 
 interface UsersPageProps {
-  posts: UserProps[];
+  users: UserProps[]; // renamed from posts to users
 }
 
-const Users: React.FC<UsersPageProps> = ({ posts }) => {
+const Users: React.FC<UsersPageProps> = ({ users }) => {
   return (
     <div className="flex flex-col h-screen">
       <Header />
-      <main className="p-4">
-        <div className="flex justify-between mb-4">
-          <h1 className="text-2xl font-semibold">User Directory</h1>
-          <button className="bg-green-600 px-4 py-2 text-white rounded-full">Add User</button>
+      <main className="p-4 flex-grow overflow-auto">
+        <div className="flex justify-between items-center">
+          <h1 className="text-2xl font-semibold">User List</h1>
+          <button className="bg-blue-700 px-4 py-2 rounded-full text-white">
+            Add User
+          </button>
         </div>
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
-          {posts?.map((user, key) => (
-            <UserCard key={key} {...user} />
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 mt-4">
+          {users.map((user, index) => (
+            <UserCard key={index} {...user} />
           ))}
         </div>
       </main>
@@ -27,11 +30,11 @@ const Users: React.FC<UsersPageProps> = ({ posts }) => {
 
 export async function getStaticProps() {
   const response = await fetch("https://jsonplaceholder.typicode.com/users");
-  const posts = await response.json();
+  const users = await response.json();
 
   return {
     props: {
-      posts,
+      users,  // renamed from posts to users
     },
   };
 }
